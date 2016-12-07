582954d0e1336ed47efa5f407fb383451b970840
Gonzalo Solera <gonzalo1796@gmail.com>
2016-12-07 03:01:06 +0100

    Implementing evaluable class

--- interpreter.hs
+++ interpreter.hs
@@ -18,7 +18,7 @@ data Command a =
 	deriving(Read)
 
 instance (Show a) => Show (NExpr a) where
-	show (Var var) = id var
+	show (Var id) = id
 	show (Const value) = show value
 	show (Plus x y) = (show x) ++ " + " ++ (show y)
 	show (Minus x y) = (show x) ++ " - " ++ (show y)
@@ -32,13 +32,13 @@ instance (Show a) => Show (BExpr a) where
 	show (Eq x y) = (show x) ++ " = " ++ (show y)
 
 instance (Show a) => Show (Command a) where
-	show (Assign var value) = (id var) ++ " := " ++ (show value) ++ "\n"
-	show (Input var) = "INPUT " ++ (id var) ++ "\n"
-	show (Print var) = "PRINT " ++ (id var) ++ "\n"
-	show (Empty var) = "EMPTY " ++ (id var) ++ "\n"
-	show (Push p value) = "PUSH " ++ (id p) ++ " " ++ (show value) ++ "\n"
-	show (Pop p var) = "POP " ++ (id p) ++ " " ++ (id var) ++ "\n"
-	show (Size p var) = "SIZE " ++ (id p) ++ " " ++ (id var) ++ "\n"
+	show (Assign id value) = id ++ " := " ++ (show value) ++ "\n"
+	show (Input id) = "INPUT " ++ id ++ "\n"
+	show (Print id) = "PRINT " ++ id ++ "\n"
+	show (Empty id) = "EMPTY " ++ id ++ "\n"
+	show (Push p value) = "PUSH " ++ p ++ " " ++ (show value) ++ "\n"
+	show (Pop p id) = "POP " ++ p ++ " " ++ id ++ "\n"
+	show (Size p id) = "SIZE " ++ p ++ " " ++ id ++ "\n"
 	show (Seq commands) = foldr (\a b -> (show a) ++ b) "" commands		
 	show (Loop expr inst) = "WHILE " ++ (show expr) ++ "\nDO" ++ (indent ("\n" ++ (show inst))) ++ "END\n"
 	show (Cond expr instIf instElse) = "IF " ++ (show expr) ++ " THEN" ++ (indent ("\n" ++ (show instIf))) ++ "ELSE" ++ (indent ("\n" ++ (show instElse))) ++ "END\n"
@@ -83,54 +83,101 @@ getValue (SymTable ((s, v):vs)) id
 	| otherwise = getValue (SymTable vs) id
 
 getNum :: SymTable a -> Ident -> Maybe a
-getNum t id = extractNum (getValue t id)
-	where
-		extractNum :: (Maybe (Value a)) -> Maybe a
-		extractNum Nothing = Nothing
-		extractNum (Just (Right p)) = Nothing
-		extractNum (Just (Left a)) = Just a
+getNum t id = case (getValue t id) of
+	(Just (Left a))	-> Just a
+	_				-> Nothing
+
+numEval :: (Either String a) -> (a -> a -> a) -> (Either String a) -> (Either String a)
+numEval (Left error) op _ = Left error
+numEval _ op (Left error) = Left error
+numEval (Right x) op (Right y) = Right (op x y)
+
+boolEval :: (Num a, Eq a) => (Either String a) -> (Bool -> Bool -> Bool) -> (Either String a) -> (Either String a)
+boolEval (Left error) op _ = Left error
+boolEval _ op (Left error) = Left error
+boolEval (Right x) op (Right y) = if op (x /= 0) (y /= 0)
+	then Right 1
+	else Right 0
 
 class Evaluable e where
 	eval :: (Num a, Ord a) => (Ident -> Maybe a) -> (e a) -> (Either String a)
-	--typeCheck :: (Ident -> String) -> (e a) -> Bool
-{-
+	typeCheck :: (Ident -> String) -> (e a) -> Bool
+
 instance Evaluable NExpr where
+	typeCheck f (Const _) = True
+	typeCheck f (Var id) = f id == "Numerical"
+	typeCheck f (Plus x y) = (typeCheck f x) && (typeCheck f y)
+	typeCheck f (Minus x y) = (typeCheck f x) && (typeCheck f y)
+	typeCheck f (Times x y) = (typeCheck f x) && (typeCheck f y)
+
 	eval f (Const value) = Right value
-	eval f (Var id) = 
+	eval f (Var id) = case f id of
+		Nothing 	-> throw Undefined
+		Just value 	-> Right value
+	eval f (Plus x y) = numEval (eval f x) (+) (eval f y)
+	eval f (Minus x y) = numEval (eval f x) (-) (eval f y)
+	eval f (Times x y) = numEval (eval f x) (*) (eval f y)
 
 instance Evaluable BExpr where
-	eval f (OR x y) = (eval f x) || (eval f y)
-	eval f (Gt x y) = (eval f x) < (eval f y)
+	typeCheck f (OR x y) = (typeCheck f x) && (typeCheck f y)
+	typeCheck f (AND x y) = (typeCheck f x) && (typeCheck f y)
+	typeCheck f (NOT x) = (typeCheck f x)
+	typeCheck f (Gt x y) = (typeCheck f x) && (typeCheck f y)
+	typeCheck f (Eq x y) = (typeCheck f x) && (typeCheck f y)
 
-eval :: NExpr a -> (Either Stirng a)
-eval expr = eval getNum expr
 
-eval :: BExpr
+	eval f (OR x y) = boolEval (eval f x) (||) (eval f y)
+	eval f (AND x y) = boolEval (eval f x) (&&) (eval f y)
+	eval f (Eq x y) = boolEval (eval f x) (==) (eval f y)
+	eval f (Gt x y) = boolEval (eval f x) (>) (eval f y)
+	eval f (NOT x) = case eval f x of
+		(Left error) 	-> Left error
+		(Right value)	-> Right (if value == 0 then 1 else 0)
 
-input :: 
+
+random :: Num a => a
+random = 4
+
+type Result a = ((Either String [a]), SymTable a, [a])
+
+concatResults :: Result a -> Result a -> Result a
+concatResults (Left error, t, input) _ = (Left error, t, input)
+concatResults _ (Left error, t, input) = (Left error, t, input)
+concatResults (Right output1, _, _) (Right output2, t, input) = (Right (output1 ++ output2), t, input)
+
+interpretCommand :: (Num a, Ord a) => SymTable a -> [a] -> Command a -> Result a
+
+interpretCommand t [] c = interpretCommand t [random] c
+
+interpretCommand t input (Seq []) = (Right [], t, input)
+--TODO test if this is efficient by: incorrect command; infinite loop
+interpretCommand t input (Seq (c:cs)) = concatResults (interpretCommand t input c) (interpretCommand t input (Seq cs))
+		
+
+interpretCommand t input (Loop cond seq) = case eval (getNum t) cond of
+	Left error -> (Left error, t, input)
+	Right value -> if value == 0
+		then (Right [], t, input)
+		else concatResults resultSeq (interpretCommand t input2 (Loop cond seq))
+		where
+			resultSeq = interpretCommand t input seq
+			(_, t, input2) = resultSeq
+
+{-
+INPUT X
+INPUT Y 
+OUTPUT Y
+WHILE X < Y DO 
+  OUTPUT X
+  y := Y + 1
+ END
 -}
 
-
-interpretCommand :: (Num a, Ord a) => SymTable a -> [a] -> Command a -> ((Either String [a]), SymTable a, [a])
-interpretCommand t input (Seq []) = (Right [], t, input)
-interpretCommand t input (Seq (c:cs)) = processResult (interpretCommand t input c) cs
-	where
-		processResult :: (Num a, Ord a) => ((Either String [a]), SymTable a, [a]) -> [Command a] -> ((Either String [a]), SymTable a, [a])
-		processResult (Left error, t, input) _ = (Left error, t, input)
-		processResult (Right output, t, input) cs = concatResult output (interpretCommand t input (Seq cs))
-
-		concatResult :: (Num a, Ord a) => [a] -> ((Either String [a]), SymTable a, [a]) -> ((Either String [a]), SymTable a, [a])
-		concatResult _ (Left error, t, input) = (Left error, t, input)
-		concatResult output1 (Right output2, t, input) = (Right (output1 ++ output2), t, input)
-interpretCommand t (x:xs) (Input id) = (Left "hello", t, (x:xs))--input (setValue t id x)
+--interpretCommand t (x:xs) (Input id) = 
 --	where
 --		input :: ((Either String [a]), SymTable a, [a])
 
 interpretProgram :: (Num a, Ord a) => [a] -> Command a -> (Either String [a])
-interpretProgram input commands = first (interpretCommand (SymTable []) input commands)
+interpretProgram input commands = result
 	where
-		first :: ((Either String [a]), SymTable a, [a]) -> (Either String [a])
-		first (result, _, _) = result
-
-
-
+		(result, _, _) = interpretCommand (SymTable []) input commands
